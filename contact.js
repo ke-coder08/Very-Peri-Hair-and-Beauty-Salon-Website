730ad4120bf308d99001632400feec12f8eb1154
ke-coder08 <ke.coder08@gmail.com>
2023-07-17 18:31:32 +0800

    init

---
+++ contact.js
@@ -0,0 +1,132 @@
+
+// Minimum Date is the Current Date
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+
+if(dd<10){
+  dd='0'+dd
+} 
+if(mm<10){
+  mm='0'+mm
+} 
+ 
+today = yyyy+'-'+mm+'-'+dd;
+document.getElementById("Date").setAttribute("min", today);
+
+//Limit the date selection
+// Get the date input field
+var dateInput = document.getElementById("Date");
+
+// Add event listener for date input
+dateInput.addEventListener("input", function() {
+  var selectedDate = new Date(this.value);
+
+  // Check if selected date is a Sunday (day of week = 0)
+  if (selectedDate.getDay() === 0) {
+    // Set the value to an empty string to prevent selection
+    this.value = "";
+    alert("Hi! We don't have any business operations on Sunday. Please select other day.");
+  }
+});
+
+
+//Setting the time
+function validateTime(selectedTime) {
+  var minTime = '09:00';
+  var maxTime = '17:00';
+  
+  if (selectedTime < minTime || selectedTime > maxTime) {
+    alert('Please select a time between 9:00 AM and 5:00 PM only.');
+    document.getElementById('Time').value = ''; // Clear the input value
+  }
+}
+
+
+//For the Buttons Services
+const Service = window.localStorage.getItem('specificservice');
+
+// document.getElementById('specificservice').textContent = Service; //in text form
+
+const dropdownvalue = document.getElementById('dd'); 
+
+if (dropdownvalue === 0){ //For Service Select Option
+
+  alert('Please select a service.');
+
+
+}
+
+if (Service === 'Hair-Haircut'){
+  dropdownvalue.value='Hair-Haircut';
+}
+else if (Service === 'Hair-HairBlower'){
+  dropdownvalue.value='Hair-HairBlower';
+}
+else if (Service === 'Hair-HairIron'){
+  dropdownvalue.value='Hair-HairIron';
+}
+else if(Service === 'Hair-HairColor'){
+  dropdownvalue.value='Hair-HairColor';
+}
+else if(Service === 'Hair-Rebond'){
+  dropdownvalue.value='Hair-Rebond';
+}
+else if(Service === 'Hair-Brazillian'){
+  dropdownvalue.value='Hair-Brazillian';
+}
+else if(Service === 'Nails-Manicure'){
+  dropdownvalue.value='Nails-Manicure';
+}
+else if(Service === 'Nails-Pedicure'){
+  dropdownvalue.value='Nails-Pedicure';
+}
+else if(Service === 'Wellness-RegularMassage'){
+  dropdownvalue.value='Wellness-RegularMassage';
+}
+else if(Service === 'Wellness-Footspa'){
+  dropdownvalue.value='Wellness-Footspa';
+}
+else if(Service === 'Beauty-HandM'){
+  dropdownvalue.value='Beauty-HandM';
+}
+else if(Service === 'Beauty-EyebrowThread'){
+  dropdownvalue.value='Beauty-EyebrowThread';
+}
+else if(Service === 'Beauty-EyelashExtension'){
+  dropdownvalue.value='Beauty-EyelashExtension';
+}
+else{
+  dropdownvalue.value='Select a Service';
+}
+
+
+// Send Button
+function checkSelection() {
+  var selectElement = document.getElementById("dd");
+  var selectedOption = selectElement.options[selectElement.selectedIndex].value;
+
+  if (selectedOption === "Select a Service") {
+      alert("Please select a Service before submitting.");
+      return false;
+  }
+
+  return true;
+}
+
+// EMAILJS.com
+function sendMail(){
+  var params = {
+    name : document.getElementById("Fname").value,
+    email : document.getElementById("Email").value,
+    service : document.getElementById("dd").value,
+    date : document.getElementById("Date").value,
+    time : document.getElementById("Time").value,
+    message : document.getElementById("Message").value,
+  } 
+
+email.js.send("service_x5oh6kn","template_f9ut58h",params).then(function (res){
+      alert("Sent Successfully!" + res.status);
+})
+}
---
+++ services.js
@@ -0,0 +1,213 @@
+function ToRedirect(){
+  window.location.href= "contact.html";
+}
+
+
+ //Buttons in Services when clicked
+ function Redirect(btnID){
+  const form = document.getElementById('form');
+  const btnhaircut = document.getElementById('btnhaircut');
+  const btnhairblower = document.getElementById('btnhairblower');
+  const btnhairiron = document.getElementById('btnhairiron');
+  
+  const form2 = document.getElementById('form2');
+  const btnhaircolor = document.getElementById('btnhaircolor');
+  const btnrebond = document.getElementById('btnrebond');
+  const btnbrazillian = document.getElementById('btnbrazillian');
+
+  const form3 = document.getElementById('form3');
+  const btnmanicure = document.getElementById('btnmanicure');
+  const btnpedicure = document.getElementById('btnpedicure');
+
+  const form4 = document.getElementById('form4');
+  const btnregularmassage = document.getElementById('btnregularmassage');
+  const btnfootspa = document.getElementById('btnfootspa');
+
+
+  const form5 = document.getElementById('form5');
+  const btnhandm = document.getElementById('btnhandm');
+  const btneyebrow = document.getElementById('btneyebrow');
+  const btneyelash = document.getElementById('btneyelash');
+
+//******************************************************************** */
+  if (btnID === 'btnhaircut'){
+    form.addEventListener('submit', function(e){
+      e.preventDefault();
+   
+      const btnServiceValue = btnhaircut.value;
+  
+      window.localStorage.setItem('specificservice',btnServiceValue);
+  
+      window.location.href= "contact.html";
+  
+    })
+  }
+
+  else if (btnID === 'btnhairblower'){
+    form.addEventListener('submit', function(e){
+      e.preventDefault();
+   
+      const btnServiceValue = btnhairblower.value;
+  
+      window.localStorage.setItem('specificservice',btnServiceValue);
+  
+      window.location.href= "contact.html";
+  
+    })
+  }
+
+  else if (btnID === 'btnhairiron'){
+    form.addEventListener('submit', function(e){
+      e.preventDefault();
+   
+      const btnServiceValue = btnhairiron.value;
+  
+      window.localStorage.setItem('specificservice',btnServiceValue);
+  
+      window.location.href= "contact.html";
+  
+    })
+  }
+    
+//********************************************************** */
+
+  else if (btnID === 'btnhaircolor'){
+    form2.addEventListener('submit', function(e){
+      e.preventDefault();
+   
+      const btnServiceValue = btnhaircolor.value;
+  
+      window.localStorage.setItem('specificservice',btnServiceValue);
+  
+      window.location.href= "contact.html";
+  
+    })
+  }
+
+
+  else if (btnID === 'btnrebond'){
+    form2.addEventListener('submit', function(e){
+      e.preventDefault();
+   
+      const btnServiceValue = btnrebond.value;
+  
+      window.localStorage.setItem('specificservice',btnServiceValue);
+  
+      window.location.href= "contact.html";
+  
+    })
+  }
+
+  else if (btnID === 'btnbrazillian'){
+    form2.addEventListener('submit', function(e){
+      e.preventDefault();
+   
+      const btnServiceValue = btnbrazillian.value;
+  
+      window.localStorage.setItem('specificservice',btnServiceValue);
+  
+      window.location.href= "contact.html";
+  
+    })
+  }
+
+//************************************************************** */
+
+else if (btnID === 'btnmanicure'){
+  form3.addEventListener('submit', function(e){
+    e.preventDefault();
+ 
+    const btnServiceValue = btnmanicure.value;
+
+    window.localStorage.setItem('specificservice',btnServiceValue);
+
+    window.location.href= "contact.html";
+
+  })
+}
+
+else if (btnID === 'btnpedicure'){
+  form3.addEventListener('submit', function(e){
+    e.preventDefault();
+ 
+    const btnServiceValue = btnpedicure.value;
+
+    window.localStorage.setItem('specificservice',btnServiceValue);
+
+    window.location.href= "contact.html";
+
+  })
+}
+
+
+//*********************************************** */
+else if (btnID === 'btnregularmassage'){
+  form4.addEventListener('submit', function(e){
+    e.preventDefault();
+ 
+    const btnServiceValue = btnregularmassage.value;
+
+    window.localStorage.setItem('specificservice',btnServiceValue);
+
+    window.location.href= "contact.html";
+
+  })
+}
+
+
+else if (btnID === 'btnfootspa'){
+  form4.addEventListener('submit', function(e){
+    e.preventDefault();
+ 
+    const btnServiceValue = btnfootspa.value;
+
+    window.localStorage.setItem('specificservice',btnServiceValue);
+
+    window.location.href= "contact.html";
+
+  })
+}
+
+ 
+//************************************************* */
+
+else if (btnID === 'btnhandm'){
+  form5.addEventListener('submit', function(e){
+    e.preventDefault();
+ 
+    const btnServiceValue = btnhandm.value;
+
+    window.localStorage.setItem('specificservice',btnServiceValue);
+
+    window.location.href= "contact.html";
+
+  })
+}
+
+else if (btnID === 'btneyebrow'){
+  form5.addEventListener('submit', function(e){
+    e.preventDefault();
+ 
+    const btnServiceValue = btneyebrow.value;
+
+    window.localStorage.setItem('specificservice',btnServiceValue);
+
+    window.location.href= "contact.html";
+
+  })
+}
+
+else if (btnID === 'btneyelash'){
+  form5.addEventListener('submit', function(e){
+    e.preventDefault();
+ 
+    const btnServiceValue = btneyelash.value;
+
+    window.localStorage.setItem('specificservice',btnServiceValue);
+
+    window.location.href= "contact.html";
+
+  })
+}
+
+}
